a2f5331f9777bd58bd073137600dc0dacb22db77
hkomatsushiro-cq <h.komatsushiro@caraquri.com>
2018-01-24 12:23:12 +0900

    Initial Commit

---
+++ MyBlockChain/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyBlockChain
+//
+//  Created by 小松代紘生 on 2018/01/24.
+//  Copyright © 2018年 小松代紘生. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

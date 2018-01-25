e192e24b3cb6af61fec6c0b6c202cc0b8e1fdf93
komatsushiro <hk.123@keio.jp>
2018-01-25 23:24:49 +0900

    ダイアログを実装

--- MyBlockChain/ViewController.swift
+++ MyBlockChain/ViewController.swift
@@ -4,8 +4,6 @@ import RxCocoa
 
 class ViewController: UIViewController {
 
-    private let myId = "me"
-    private let recipientId = "someone"
     private let server = BlockChainServer()
 
     private let logView: UITextView! = {
@@ -31,6 +29,108 @@ class ViewController: UIViewController {
         return mineButton
     }()
 
+    private lazy var sendingDialog: UIAlertController = {
+        let sendingDialog = UIAlertController(title:"coinを送信する",
+                                           message: "項目を入力してください",
+                                           preferredStyle: .alert)
+        let cancelAction = UIAlertAction(title: "キャンセル", style: .cancel, handler: nil)
+        let okAction = UIAlertAction(title: "送信", style: .default) {
+            action -> Void in
+            let textFields = sendingDialog.textFields as Array<UITextField>?
+            guard let values = textFields else {
+                return
+            }
+            for textField in values {
+                if (textField.text ?? "").isEmpty {
+                    self.present(self.alertDialog, animated: true, completion: nil)
+                    return
+                }
+            }
+            let index = self.server.send(sender: values[0].text!,
+                                         recipient: values[1].text!,
+                                         amount: Int(values[2].text!)!)
+            let text = "Transaction will be added to Block \(index)"
+            self.logView.text = text + "\n" + self.logView.text
+            self.updateChain()
+        }
+        sendingDialog.addAction(cancelAction)
+        sendingDialog.addAction(okAction)
+        sendingDialog.addTextField { textField -> Void in
+            textField.placeholder = "送信者"
+            var label = UILabel(frame: CGRect(x: 0, y: 0, width: 50, height: 30))
+            label.text = "From"
+            textField.leftView = label
+            textField.leftViewMode = .always
+        }
+        sendingDialog.addTextField { textField -> Void in
+            textField.placeholder = "送信先"
+            var label = UILabel(frame: CGRect(x: 0, y: 0, width: 50, height: 30))
+            label.text = "To"
+            textField.leftView = label
+            textField.leftViewMode = .always
+        }
+        sendingDialog.addTextField { textField -> Void in
+            textField.placeholder = "金額"
+            textField.keyboardType = .decimalPad
+            var label = UILabel(frame: CGRect(x: 0, y: 0, width: 50, height: 30))
+            label.text = "$"
+            textField.leftView = label
+            textField.leftViewMode = .always
+        }
+
+        return sendingDialog
+    }()
+
+    private lazy var miningDialog: UIAlertController = {
+        let miningDialog = UIAlertController(title: "マイニングする",
+                                             message: "あなたのIDを入力してください",
+                                             preferredStyle: .alert)
+        let cancelAction = UIAlertAction(title: "キャンセル", style: .cancel, handler: nil)
+        let okAction = UIAlertAction(title: "採掘", style: .default) {
+            action -> Void in
+            let textFields = miningDialog.textFields as Array<UITextField>?
+            guard let values = textFields else {
+                return
+            }
+            for textField in values {
+                if (textField.text ?? "").isEmpty {
+                    self.present(self.alertDialog, animated: true, completion: nil)
+                    return
+                }
+            }
+            let startTime = CACurrentMediaTime()
+            let text = "Mining..."
+            self.logView.text = text + "\n" + self.logView.text
+
+            self.server.mine(recipient: values[0].text!, completion: { (block) in
+                let text = String(format: "New Block Forged (%.1f s)", CACurrentMediaTime() - startTime)
+                self.logView.text = text + "\n" + self.logView.text
+                print(text+block.description())
+                self.updateChain()
+            })
+        }
+        miningDialog.addAction(cancelAction)
+        miningDialog.addAction(okAction)
+        miningDialog.addTextField { textField -> Void in
+            textField.placeholder = "ID"
+            var label = UILabel(frame: CGRect(x: 0, y: 0, width: 50, height: 30))
+            label.text = "ID"
+            textField.leftView = label
+            textField.leftViewMode = .always
+        }
+        return miningDialog
+    }()
+
+    private lazy var alertDialog: UIAlertController = {
+        let alertDialog = UIAlertController(title:"エラー",
+                                           message: "全ての項目を入力してください",
+                                           preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default) { action -> Void in
+        }
+        alertDialog.addAction(okAction)
+        return alertDialog
+    }()
+
     private let disposeBag = DisposeBag()
 
     override func viewDidLoad() {
@@ -42,23 +142,11 @@ class ViewController: UIViewController {
         updateChain()
 
         sendButton.rx.tap.subscribe(onNext: { [unowned self] _ in
-            let index = self.server.send(sender: self.myId, recipient: self.recipientId, amount: 5)
-            let text = "Transaction will be added to Block \(index)"
-            self.logView.text = text + "\n" + self.logView.text
-            self.updateChain()
+            self.present(self.sendingDialog, animated: true, completion: nil)
         }).disposed(by: disposeBag)
 
         mineButton.rx.tap.subscribe(onNext: { [unowned self] _ in
-            let startTime = CACurrentMediaTime()
-            let text = "Mining..."
-            self.logView.text = text + "\n" + self.logView.text
-
-            self.server.mine(recipient: self.myId, completion: { (block) in
-                let text = String(format: "New Block Forged (%.1f s)", CACurrentMediaTime() - startTime)
-                self.logView.text = text + "\n" + self.logView.text
-                print(text+block.description())
-                self.updateChain()
-            })
+            self.present(self.miningDialog, animated: true, completion: nil)
         }).disposed(by: disposeBag)
     }
 
@@ -103,4 +191,3 @@ class ViewController: UIViewController {
     }
 
 }
-
